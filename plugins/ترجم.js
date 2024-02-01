212106c50add74555aac813d0030bd14be5a96e5
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-01 12:58:47 +0300

    Create ترجم.js

---
+++ plugins/ترجم.js
@@ -0,0 +1,43 @@
+
+import translate from '@vitalets/google-translate-api'
+const defaultLang = 'en'
+const tld = 'cn'
+
+let handler = async (m, { args, usedPrefix, command }) => {
+    let err = `
+💝 Quteenah 💝
+         Translator
+
+🔊 مثال :
+
+.translate ar مرحبا
+.translate en Hello World
+ 
+                   اللغــات: 
+..
+زر هذا الرابط لتعرف جنيع اختصارات اللغات 
+
+https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    try {
+       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
+       m.reply(result.text)
+    } catch (e) {
+        throw err
+    } 
+
+}
+handler.help = ['trad <leng> <text>']
+handler.tags = ['tools']
+handler.command = ['ترجمه', 'ترجم']
+
+export default handler
