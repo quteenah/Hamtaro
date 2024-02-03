08f8789b92b3c8140b37ff9f47426ada192d1241
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-03 12:53:37 +0300

    Create دائري.js

---
+++ plugins/دائري.js
@@ -0,0 +1,18 @@
+import uploadImage from '../lib/uploadImage.js';
+import {sticker} from '../lib/sticker.js';
+const handler = async (m, {conn, text}) => {
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || '';
+    const img = await q.download();
+    const url = await uploadImage(img);
+    const scircle = global.API('dzx', '/api/canvas/circle', {url});
+    const stiker = await sticker(null, scircle, global.packname, global.author);
+    conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, {asSticker: true});
+  } catch (e) {
+    m.reply('*[❗خطأ❗] قم برد على الصوره لتحويلها لملصق دائري*');
+  }
+};
+handler.command = /^ستيكر|ستيك$/i;
+export default handler;
+/* `https://api.dhamzxploit.my.id/api/canvas/circle?url=${url}` */
