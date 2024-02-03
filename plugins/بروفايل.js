1231671a78f5de46142e3cc85013a186c48e3d25
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-03 10:55:39 +0300

    Create بروفايل.js

---
+++ plugins/بروفايل.js
@@ -0,0 +1,30 @@
+import {createHash} from 'crypto';
+import PhoneNumber from 'awesome-phonenumber';
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
+  let pp = 'https://telegra.ph/file/06cc652844ea19e8aed1c.jpg';
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en mi base de datos`;
+  try {
+    pp = await conn.profilePictureUrl(who);
+  } catch (e) {
+  } finally {
+    const {name, limit, lastclaim, registered, regTime, age, premiumTime} = global.db.data.users[who];
+    const username = conn.getName(who);
+    const prem = global.prems.includes(who.split `@` [0]);
+    const sn = createHash('md5').update(who).digest('hex');
+    const str = `*الاسـم:* ${username} ${registered ? '(' + name + ') ': ''}
+*الرقـم:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*الرابـط:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
+*الحد:* ${limit} استخداما
+*مسجل:* ${registered ? 'Si': 'No'}
+*بريميوم:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
+*الرقم التسلسلي:* 
+${sn}`;
+    conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
+  }
+};
+handler.help = ['profile [@user]'];
+handler.tags = ['xp'];
+handler.command = /^بروفايل|profile?$/i;
+export default handler;
