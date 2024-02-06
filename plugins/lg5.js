15f9538e9f087421f68bae86d46b42b4a0dce52c
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-06 12:25:20 +0300

    Update lg5.js

--- plugins/lg5.js
+++ plugins/lg5.js
@@ -1,12 +1,12 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw 'مثال :\n*.lg5* bobiza'
+  if (!args[0]) throw 'مثال :\n*.لوجو5* علي'
   m.reply('_إنتظر..._')
   let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/noureddine_ouafy`, m, false)
+  conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/ali_quteenah`, m, false)
 }
 handler.help = ['flaming5'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
-handler.command = /^(lg5)$/i
+handler.command = /^(لوجو5)$/i
 export default handler
