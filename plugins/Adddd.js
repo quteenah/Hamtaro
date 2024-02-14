2555ffc2c274a0ae4a669e0a9bddd42907500c53
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-15 02:59:36 +0300

    Update Adddd.js

--- plugins/Adddd.js
+++ plugins/Adddd.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'What do you want to create?'
-    m.react(rwait)
+    m.react(done)
     let msg = encodeURIComponent(text)
     let res = await fetch(`https://aemt.me/bingimg?text=${msg}`)
     let data = await res.json()
