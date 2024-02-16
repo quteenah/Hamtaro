7a586c95b9a86484390ad2a47d4a98348d15e388
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-16 03:22:28 +0300

    Update Add.js

--- plugins/Add.js
+++ plugins/Add.js
@@ -1,50 +1,24 @@
-
-import displayLoadingScreen from '../lib/loading.js'
-import fetch from 'node-fetch'
-import {delay} from '@whiskeysockets/baileys'
-
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  try {
-    if (!text) throw `uhm.. what do you want to say?`
-    m.react('🤖')
-    //await displayLoadingScreen(conn, m.chat)
-
-
-    const prompt = encodeURIComponent(text);
-    let apiurl = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`
-
-    const result = await fetch(apiurl);
-    const response = await result.json();
-    console.log(response)
-    const textt = response.result.reply;
-    await typewriterEffect(conn,m, m.chat , textt);
-       
-  } catch (error) {
-    console.error(error);
-    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
-  }
-}
-handler.help = ['gemini <text>']
-handler.tags = ['tools']
-handler.command = /^(gpt4)$/i
-
-export default handler
-
-async function typewriterEffect(conn, quoted ,from, text) {
-    let { key } = await conn.sendMessage(from, { text: 'Thinking...' } , {quoted:quoted})
-  
-    for (let i = 0; i < text.length; i++) {
-      const noobText = text.slice(0, i + 1);
-      await conn.relayMessage(from, {
-        protocolMessage: {
-          key: key,
-          type: 14,
-          editedMessage: {
-            conversation: noobText
-          }
-        }
-      }, {});
-   
-       await delay(100); // Adjust the delay time (in milliseconds) as needed
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let regex = /x/g
+    if (!text) throw 'Give a number to search'
+    if (!text.match(regex)) throw `*Example: ${usedPrefix + command} 91760590201x*`
+    let random = text.match(regex).length, total = Math.pow(10, random), array = []
+    for (let i = 0; i < total; i++) {
+    let list = [...i.toString().padStart(random, '0')]
+    let result = text.replace(regex, () => list.shift()) + '@s.whatsapp.net'
+    if (await conn.onWhatsApp(result).then(v => (v[0] || {}).exists)) {
+    let info = await conn.fetchStatus(result).catch(_ => {})
+    array.push({ exists: true, jid: result, ...info })
+    } else {
+    array.push({ exists: false, jid: result })
+    }}
+    let txt = 'Registered\n\n' + array.filter(v => v.exists).map(v => `• Link: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'descripiion'}\n*• set on:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*Not registered*\n\n' + array.filter(v => !v.exists).map(v => v.jid.split('@')[0]).join('\n')
+    m.reply(txt)
     }
-         }
+    handler.help = ['nowa']
+handler.tags = ['tools']
+    handler.command = /^nowa$/i
+    export default handler
+    function formatDate(n, locale = 'in') {
+    let d = new Date(n)
+    return d.toLocaleDateString(locale, { timeZone: 'Asia/Kolkata' })}
