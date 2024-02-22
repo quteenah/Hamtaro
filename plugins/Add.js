d41cb3e4470c5877485f46917628ce6a6e8baaba
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-22 04:03:34 +0300

    Update Add.js

--- plugins/Add.js
+++ plugins/Add.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async (m, {text, args}) => {
   if (!args[0]) throw `Use example .simi halo`
   let api = await fetch(`https://api.clayzaaubert.my.id/api/ai/simi?kata=${text}&apikey=${global.clayza}`)
-  let res = await api.json()
+
   m.reply(res.message)
 }
 handler.command = ['simi']
