8c3d0f791e10b47e5bf1e2df450983baf7627a2f
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-02-22 04:15:38 +0300

    Update Add.js

--- plugins/Add.js
+++ plugins/Add.js
@@ -1,11 +1,15 @@
-import fetch from 'node-fetch'
-let handler = async (m, {text, args}) => {
-  if (!args[0]) throw `Use example .simi halo`
-  let api = await fetch(`https://api.clayzaaubert.my.id/api/ai/simi?kata=${text}&apikey=${global.clayza}`)
-}
-handler.command = ['simi']
-handler.tags = ['ai']
-handler.help = ['simi']
-handler.limit = true;
+import fetch from "node-fetch"
 
+let handler = async (m, { conn, isOwner, usedPrefix, command, text }) => {
+if (!text) throw 'Masukan Promptnya\nExample:\n.animedif neko girl, cute face, upper body, white hair, look at viewer'
+m.reply(wait)
+let anu = `https://api.clayzaaubert.my.id/api/ai/animediffusion?q=${text}&apikey=${global.clayza}`
+conn.sendFile(m.chat, anu, 'anu.jpg', `Prompt: ${text}`, m)
+    
+}
+handler.help = ['animedif']
+handler.tags = ['ai']
+handler.command = /^(animedif)$/i
+handler.limit = true
+handler.premium = true
 export default handler
