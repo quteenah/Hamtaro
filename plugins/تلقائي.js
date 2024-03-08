8a308f789716e394e1a506f6967d1e4eb001f929
quteenah <160974379+quteenah@users.noreply.github.com>
2024-03-09 00:17:16 +0300

    Update تلقائي.js

--- plugins/تلقائي.js
+++ plugins/تلقائي.js
@@ -1,10 +1,38 @@
-let handler = async (m, { conn }) => {
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 like you only belong in jail 🚨🚔*', m)
-    }
-    handler.help = ['lolicon']
-    handler.tags = ['maker']
-    handler.command = /^(lolicon)$/i
-    export default handler
+
+import fetch from 'node-fetch'
+import { mediafiredl } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
+	var limit
+     if((isOwner || isPrems)) limit = 1200
+     else limit = 100
+   if (!args[0]) throw `✳️ Enter the mediafire link next to the command`
+    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`
+    m.react(rwait)
+    let full = /f$/i.test(command)
+    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+    let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer()
+    let res = await mediafiredl(args[0])
+    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
+    let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
+    let caption = `
+   ≡ *MEDIAFIRE*
+
+▢ *Number:* ${filename}
+▢ *Size:* ${filesizeH}
+▢ *Extension:* ${ext}
+▢ *Uploaded:* ${aploud}
+${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade to premium to be able to download files more than *900 MB*` : ''} 
+`.trim()
+    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
+    
+    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+    m.react(done)
+}
+handler.help = ['mediafire <url>']
+handler.tags = ['downloader', 'premium']
+handler.command = ['mediafire', 'mfire'] 
+handler.credit = true
+handler.premium = false
+
+export default handler
