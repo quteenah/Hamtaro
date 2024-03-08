d0c1de982c4b6ba0d39d1d8625b47c9d9e9a5fc6
quteenah <160974379+quteenah@users.noreply.github.com>
2024-03-08 23:47:39 +0300

    Update تلقائي.js

--- plugins/تلقائي.js
+++ plugins/تلقائي.js
@@ -1,23 +1,29 @@
-let handler  = async (m, { conn }) => { 
- let name = conn.getName(m.sender) 
- let teks = ` 
- ${pickRandom([`
-  ⚠️*سيتم نقل البوت الى الرقم الجديد👇🏻👇🏻👇🏻*
-+966558513424
-*كل ماعليك سوا الظغط ع الرقم*
-*اذا كنت محظور من البوت الثاني*
-*تواصل مع المطور لرفع الحظر*
-👇🏻
-+966578700193
-╯ ───═┅─┅─┅═─── ╰`])} 
- `.trim() 
- conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }}) 
- } 
- handler.customPrefix = /علي|المطور|.|مرحبا|بوت/i 
- handler.command = new RegExp 
-  
- export default handler 
-  
- function pickRandom(list) { 
-     return list[Math.floor(Math.random() * list.length)] 
- }
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m, {conn, text, args, usedPrefix, command}) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || q.mediaType || '';
+  if (!/image/g.test(mime)) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*';
+  m.reply('*[❗] 𝙲𝙾𝙽𝚅𝙸𝙴𝚁𝚃𝙸𝙴𝙽𝙳𝙾 𝙸𝙼𝙰𝙶𝙴𝙽 𝙰 𝙳𝙸𝚂𝙴𝙽̃𝙾 𝙰𝙽𝙸𝙼𝙴, 𝚂𝙴𝙰 𝙿𝙰𝙲𝙸𝙴𝙽𝚃𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾*');
+  const data = await q.download?.();
+  const image = await uploadImage(data);
+  try {
+    const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
+    await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
+  } catch (i) {
+    try {
+      const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
+      await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
+    } catch (a) {
+      try {
+        const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
+        await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
+      } catch (e) {
+        throw '*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝙴𝚁𝙸𝙵𝙸𝚀𝚄𝙴 𝚀𝚄𝙴 𝙴𝙽 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚂𝙴𝙰 𝚅𝙸𝚂𝙸𝙱𝙻𝙴 𝙴𝙻 𝚁𝙾𝚂𝚃𝚁𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰*';
+      }
+    }
+  }
+};
+handler.help = ['toanime'];
+handler.tags = ['tools'];
+handler.command = /^(jadianime|toanime)$/i;
+export default handler;
