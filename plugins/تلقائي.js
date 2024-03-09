0ad82e4568a77497d087d1a282f65bdaf0a7b6cf
quteenah <160974379+quteenah@users.noreply.github.com>
2024-03-09 12:44:39 +0300

    Update تلقائي.js

--- plugins/تلقائي.js
+++ plugins/تلقائي.js
@@ -1,10 +1,14 @@
+//By Shirokami Ryzen
+//Dont delete this credit!!!
 import fetch from 'node-fetch'
 import { pinterest } from '../lib/scrape.js'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `*تحميل الصور من pinterest* سوف يرسل لك اكثر من 5 صور ذاك جودة عالية\n\n*مثال:*\n*${usedPrefix + command} boy*`
+  conn.reply(m.chat, 'انتظر قليلا...', m)
 
   try {
-    const hasil = await pinterest('Jujutsu Kaisen 4K');
+    const hasil = await pinterest(text);
     let gambarUrls = hasil.slice(0, 20); // Ambil 20 gambar pertama
 
     // Mengacak array gambarUrls
@@ -14,24 +18,24 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
     }
 
     // Mengirim 10 gambar secara acak
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       let imageUrl = gambarUrls[i];
       let imageRes = await fetch(imageUrl);
       let imageBuffer = await imageRes.buffer();
 
       // Menggunakan fungsi sendImage untuk mengirim gambar ke WhatsApp
-      await conn.sendFile(m.chat, imageBuffer, 'chainsaw-man.jpg', '');
+      await conn.sendFile(m.chat, imageBuffer, 'bobiza.jpg', '');
 
       // Tambahkan jeda agar tidak mengirim gambar terlalu cepat
       await new Promise(resolve => setTimeout(resolve, 500));
     }
   } catch (e) {
     console.log(e)
-    conn.reply(m.chat, 'Error', m)
+    conn.reply(m.chat, '*حدث خطأ أثناء تنزيل الصورة*.', m)
   }
 }
 
-handler.help = ['jujutsu']
-handler.tags = ['anime']
-handler.command = /^jujutsu$/i
+handler.help = ['pinterest2']
+handler.tags = ['downloader']
+handler.command = /^pinterest2$/i
 export default handler
