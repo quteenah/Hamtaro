c9ca6b02be08c8cb77478cb51ff5948b61956daf
quteenah <160974379+quteenah@users.noreply.github.com>
2024-04-03 05:35:00 +0300

    Update Add.js

--- plugins/Add.js
+++ plugins/Add.js
@@ -1,20 +1,25 @@
-import {googleImage} from '@bochilteam/scraper';
+import uploadImage from '../lib/uploadImage.js';
+import {sticker} from '../lib/sticker.js';
 
-const handler = async (m, {conn, text, usedPrefix, command}) => {
+
+const handler = async (m, {conn, text}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.downloader_imagen
+  const tradutor = _translate.plugins.sticker_scircle
 
-
-  if (!text) throw `${tradutor.texto1} ${usedPrefix + command} Minecraft*`;
-  //if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾 𝙿𝚄𝙴𝙳𝙾 𝙴𝙽𝚅𝙸𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙽𝚃𝙴𝙽𝙸𝙴𝙽𝙳𝙾 𝙴𝚂𝚃𝙰 𝙿𝚁𝙾𝙷𝙸𝙱𝙸𝙳𝙾 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾\n𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny');
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `${tradutor.texto2[0]} ${text}\n${tradutor.texto2[1]} ${link}\n${tradutor.texto2[2]}`, m);
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || '';
+    const img = await q.download();
+    const url = await uploadImage(img);
+    const scircle = global.API('dzx', '/api/canvas/circle', {url});
+    const stiker = await sticker(null, scircle, global.packname, global.author);
+    conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, {asSticker: true});
+  } catch (e) {
+    m.reply(tradutor.texto1);
+  }
 };
-handler.help = ['gimage <query>', 'imagen <query>'];
-handler.tags = ['internet', 'tools'];
-handler.command = /^(gimage|image|imagen)$/i;
+handler.command = /^scircle|circle$/i;
 export default handler;
+/* `https://api.dhamzxploit.my.id/api/canvas/circle?url=${url}` */
