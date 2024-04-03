6801e931010c865a8cf254a3f0b2e7fd24eaf37e
quteenah <160974379+quteenah@users.noreply.github.com>
2024-04-03 06:13:52 +0300

    Update Ad.js

--- plugins/Ad.js
+++ plugins/Ad.js
@@ -1,39 +1,17 @@
+import moment from 'moment-timezone';
 
-import axios from "axios"
+const handler = async (m, {conn}) => {
 
-let handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
-	let query = "هذا الامر خاص بتحميل الصور من موقع\n\nunsplash.com/\n\n نكتب هكدا على سبيل المثال: \n*.unsplash* man"
-	let text
-	if (args.length >= 1) {
-		text = args.slice(0).join(" ")
-	} else if (m.quoted && m.quoted.text) {
-		text = m.quoted.text
-	} else throw query
-	
-	try {
-	m.reply(wait)
-	 var imge = await searchImages(text)
-	 var xmg = imge.getRandom()
-	 
-	 conn.sendFile(m.chat, xmg.urls.full ? xmg.urls.regular : xmg.urls.thumb, "النتيجة", "♥" + xmg.description + "*", m)
-   } catch (e) {
-   throw 'error'
- }
-}
-handler.help = ["unsplash"]
-handler.tags = ["downloader"]
-handler.command = /^(unsplash)$/i
-export default handler
+  const tzAF = moment().tz('Asia/Sanaa').format('DD/MM HH:mm');
+  await conn.sendMessage(m.chat, {text: `\`\`\`
+الوقت الحالي في اليمن هو :
 
-async function searchImages(term) {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
-    headers: {
-      Authorization: "Client-ID mxr-J3YtqewQPrikLf7npmJY7ZvKKcxg7erlUer4bJM",
-    },
-    params: {
-      query: term,
-    },
-  });
 
-  return response.data.results;
-};
+▢ morocco     : ${tzAF}
+  ${String.fromCharCode(8206).repeat(850)}
+  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+  };
+handler.help = ["tz"]
+handler.tags = ["infobot"]
+handler.command = /^(tz)$/i
+  export default handler;
