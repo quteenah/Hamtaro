e5dd329316d2f87f4c14f039bda8a39f37e3868e
quteenah <160974379+quteenah@users.noreply.github.com>
2024-04-04 13:19:20 +0300

    Update Ad.js

--- plugins/Ad.js
+++ plugins/Ad.js
@@ -1,114 +1,73 @@
-import fetch from 'node-fetch';
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  try {
-    const pp = imagen4;
-    // let vn = './media/menu.mp3'
-    const img = './Menu2.jpg';
-    const d = new Date(new Date + 3600000);
-    const locale = 'ar';
-    const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
-    const _uptime = process.uptime() * 1000;
-    const uptime = clockString(_uptime);
-    const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, limit, level, role} = global.db.data.users[m.sender];
-    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
-    const rtotal = Object.entries(global.db.data.users).length || '0'
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `
-╮───────────────╭ـ 
-│⚝ ⃟✨❯ *اهلا بك, ${taguser}*
-︎╯───────────────╰ـ
-*╮ ───═┅─┅─┅═─── ╭*
-│⚝ ⃟👤❯ *المالك:* علـي قطينـة
-│⚝ ⃟🤖❯ *رقم البوت:* wa.me/966558513424
-│⚝ ⃟🗓️❯ *التاريخ:* ${date}
-│⚝ ⃟⏲️❯ *الوقت:* ${uptime}
-│⚝ ⃟👤❯ *المستخدم:* ${rtotalreg}
-│⚝ ⃟👥❯ *عدد المستخدمين:* ${rtotal}
-*╯ ───═┅─┅─┅═─── ╰*
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+try {
+let vn = './media/menu3.mp3'
+let pp = imagen4
+let img = await(await fetch('https://www.instagram.com/ali_quteenah')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'ar'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `
 ╮───────────────╭ـ
-│「❥📲 *حسـابك* 📃❥」
-︎╯───────────────╰ـ
-*╮ ───═┅─┅─┅═─── ╭*
-│⚝ ⃟🎖️❯ *المستوى:* ${level}
-│⚝ ⃟🧰❯ *الخبرات:* ${exp}
-│⚝ ⃟⚓❯ *رتـبة:* ${role}
-│⚝ ⃟💎❯ *المـاس:* ${limit}
-│⚝ ⃟👾❯ *العمـلات:* ${money}
-│⚝ ⃟🪙❯ *الرموز:* ${joincount}
-│⚝ ⃟🎟️❯ *المميز:* ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
-│
-*╯ ───═┅─┅─┅═─── ╰*
-${readMore}
-${(conn.user.jid == global.conn.user.jid ? '' : `Jadibot de: https://wa.me/${global.conn.user.jid.split`@`[0]}`) || ''}
-╮───────────────╭ـ 
 │「❥❤️ *إسلامية* ✨👑❥」
+│───────────────╰ـ
+*╮ ───═┅─┅─┅═─── ╭*
+*│⚝ ⃟✨❯ ${usedPrefix}الله*
+*│⚝ ⃟✨❯ ${usedPrefix}سوره*
+*│⚝ ⃟✨❯ ${usedPrefix}السوره*
+*│⚝ ⃟✨❯ ${usedPrefix}القران*
+*│⚝ ⃟✨❯ ${usedPrefix}قران*
+*│⚝ ⃟✨❯ ${usedPrefix}اذكار المساء*
+*│⚝ ⃟✨❯ ${usedPrefix}اذكار الصباح*
+*│⚝ ⃟✨❯ ${usedPrefix}ادعيه*
+*│⚝ ⃟✨❯ ${usedPrefix}ايه*
 ︎╯───────────────╰ـ
-*│⚝ ⃟📿❯${usedPrefix}اسلامية*
-╮───────────────╭ـ
-│「❥🗣️ *الذكاء الاصطناعي* 📃❥」
-│───────────────╰ـ
-│⚝ ⃟🗣️❯${usedPrefix}ذكاء*
-╮───────────────╭ـ
-│「❥⚙️ *الـبـحـث* 📊❥」
-│───────────────╰ـ
-│⚝ ⃟📊❯${usedPrefix}البحث*
-╮───────────────╭ـ
-│「❥⚙️ *التنـزيلات* ✨👑❥」
-│───────────────╰ـ
-│⚝ ⃟📲❯${usedPrefix}التنزيلات*
-╮───────────────╭ـ
-│「❥⚙️ *التطبيقـات* 🎭🥏❥」
-│───────────────╰ـ
-│⚝ ⃟📱❯${usedPrefix}التطبيقات*
-╮───────────────╭ـ
-│「❥⚙️ *ملصقات* 🧸🖼️❥」
-│───────────────╰ـ
-│⚝ ⃟🧸❯${usedPrefix}الملصقات*
-╮───────────────╭ـ
-│「❥⚙️ *صور عشوائية* 🖼️❥」
-│───────────────╰ـ
-│⚝ ⃟🖼️❯${usedPrefix}عشوائي*
-╮───────────────╭ـ
-│「❥⚙️ *تغيرالصوت* 🎤❥」
-│───────────────╰ـ
-│⚝ ⃟🎤❯${usedPrefix}الصوتيات*
-╮───────────────╭ـ
-│「❥⚙️ *اوامر اضافية* ✨👑❥」
-│───────────────╰ـ
-│⚝ ⃟♥️❯${usedPrefix}اخرى*
-╮───────────────╭ـ
-│「❥⚙️ *اوامر إنجليزية* 🆎❥」
-│───────────────╰ـ
-│⚝ ⃟🅰️❯${usedPrefix}انجلش*
-*╯ ───═┅─┅─┅═─── ╰*`.trim();
-    if (m.isGroup) {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
-    } else {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
-    }
-  } catch {
-    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
-  }
-};
-handler.command = /^(menu|اوامر|أوامر|الاوامر|مساعده|info|الأوامر|القائمة|القائمه|معلومات|تشغيل|الو|مرحبا|امر)$/i;
-handler.exp = 50;
-handler.fail = null;
-export default handler;
+`.trim()
+let buttonMessage = {
+image: pp,
+caption: str.trim(),
+mentions: [m.sender],
+footer: `*${wm}*`,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: '『👑𝕜𝕚𝕟𝕘┇🤖┇𝕓𝕠𝕥┇👑』',
+body: null,
+thumbnail: img,
+sourceUrl: `https://www.instagram.com/ali_quteenah`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+} catch {
+conn.reply(m.chat, '*القائمة بها خطأ  ، يرجى إخبار مالك الروبوت بذلك‼️❗*', m)
+}}
+handler.command = /^(المزيد|m|a|انجليزي|help|info|انجلش|انجليش|إنجليزي|قائمة|الاوامر2|القائمة2|menu2|ali)$/i
+handler.exp = 50
+handler.fail = null
+export default handler
 function clockString(ms) {
-  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
-  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
