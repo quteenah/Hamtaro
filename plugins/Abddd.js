c0b4f862af564f302aad2a4f98b085b8356331c3
quteenah <160974379+quteenah@users.noreply.github.com>
2024-04-10 18:58:31 +0300

    Create Abddd.js

---
+++ plugins/Abddd.js
@@ -0,0 +1,26 @@
+/**
+Ini wm ku jika dihapus mandul kalian😂🔥
+by: Tio 
+**/
+
+import axios from "axios"
+
+let handler = async (m, {text, usedPrefix, command}) => {
+if (!text) return m.reply('Masukan url')
+try {
+await m.react('⌛')
+let { data } = await axios.post(`https://v3.igdownloader.app/api/ajaxSearch?recaptchaToken=null&q=${text}&t=media&lang=en`)
+let $ = cheerio.load(data.data)
+let result = $('div >  div > a').attr('href')
+await conn.sendFile(m.chat, result, '', 'Instagram: '+text, m)
+await m.react('✅')
+} catch (e) {
+throw eror
+}
+}
+handler.help = ['instagram3']
+handler.tags = ['downloader']
+handler.limit = true
+handler.command = /^(i(nsta)?g(ram)?3)$/i
+
+export default handler
